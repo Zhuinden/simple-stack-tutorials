bace8b7b00abcf1c66af0a9a5e91665c8746cdf5
zhuinden <zhuinden@gmail.com>
2020-01-20 10:08:24 +0100

    Use SimpleStateChanger for newest example

--- app/src/main/java/com/zhuinden/simplestacktutorials/steps/step_6/Step6Activity.kt
+++ app/src/main/java/com/zhuinden/simplestacktutorials/steps/step_6/Step6Activity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.zhuinden.simplestack.History
 import com.zhuinden.simplestack.StateChange
-import com.zhuinden.simplestack.StateChanger
 import com.zhuinden.simplestack.navigator.Navigator
 import com.zhuinden.simplestacktutorials.R
+import com.zhuinden.simplestacktutorials.utils.SimpleStateChanger
 import kotlinx.android.synthetic.main.activity_step6.*
 
-class Step6Activity : AppCompatActivity(), StateChanger {
+class Step6Activity : AppCompatActivity(), SimpleStateChanger.NavigationHandler {
     private lateinit var fragmentStateChanger: FragmentStateChanger
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class Step6Activity : AppCompatActivity(), StateChanger {
         fragmentStateChanger = FragmentStateChanger(supportFragmentManager, R.id.step6Root)
 
         Navigator.configure()
-            .setStateChanger(this)
+            .setStateChanger(SimpleStateChanger(this))
             .install(this, step6Root, History.of(Step6FirstScreen()))
     }
 
@@ -29,13 +29,7 @@ class Step6Activity : AppCompatActivity(), StateChanger {
         }
     }
 
-    override fun handleStateChange(stateChange: StateChange, completionCallback: StateChanger.Callback) {
-        if (stateChange.isTopNewKeyEqualToPrevious) {
-            completionCallback.stateChangeComplete()
-            return
-        }
-
+    override fun handleNavigationEvent(stateChange: StateChange) {
         fragmentStateChanger.handleStateChange(stateChange)
-        completionCallback.stateChangeComplete()
     }
 }
