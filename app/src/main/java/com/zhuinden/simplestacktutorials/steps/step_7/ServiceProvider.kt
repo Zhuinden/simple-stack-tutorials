1a4fb5175039f7e7ef3051491f109aa017360990
Zhuinden <zhuinden@gmail.com>
2020-01-21 04:48:41 +0100

    Ensure that key's own services are not registered into a parent scope

--- app/src/main/java/com/zhuinden/simplestacktutorials/steps/step_7/ServiceProvider.kt
+++ app/src/main/java/com/zhuinden/simplestacktutorials/steps/step_7/ServiceProvider.kt
@@ -11,11 +11,11 @@ class ServiceProvider : ScopedServices {
     override fun bindServices(serviceBinder: ServiceBinder) {
         val key = serviceBinder.getKey<FragmentKey>()
 
-        if (key is HasServices) {
+        val scope = serviceBinder.scopeTag
+
+        if (key is HasServices && key.scopeTag == scope) {
             key.bindServices(serviceBinder) // screen-bound shared services
         }
-
-        val scope = serviceBinder.scopeTag
 
         with(serviceBinder) {
             when (scope) { // explicit shared services
